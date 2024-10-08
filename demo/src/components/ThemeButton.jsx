9444e08020a66a3fc386f31d5f0b46e2f509a9d8
Alex Balak <alex.balak@outlook.com>
2024-10-09 00:19:36 +0200

    working fine

--- demo/src/components/ThemeButton.jsx
+++ demo/src/components/ThemeButton.jsx
@@ -8,7 +8,7 @@ function ThemeButton({darkMode, setDarkMode}) {
       data-tooltip-target="default-card-example-toggle-dark-mode-tooltip"
       type="button"
       data-toggle-dark="dark"
-      className="position: absolute top-3 right-3 flex items-center w-9 h-9 justify-center text-xs font-medium text-cardTextColor bg-cardBackgroundColor border border-cardBorderColor rounded-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-gray-300">
+      className="position: absolute top-3 right-3 flex items-center w-9 h-9 justify-center text-xs font-medium text-cardTextColor bg-cardBackgroundColor border-2 border-gray-200 rounded-lg hover:bg-gray-100 hover:text-blue-700">
       <MoonSunIcon darkMode={darkMode} />
       <span className="sr-only">Toggle dark/light mode</span>
     </button>
